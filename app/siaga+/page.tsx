6b9b8757898c6f6e8f96b441c20d70efaf336439
Nadineapriliaputri <nnadin1804@gmail.com>
2026-06-15 17:04:39 +0800

    pesan commit

--- app/siaga+/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function holidaypage() {
-    return ( <div></div>);
-        
-    
-}
---
+++ app/siaga/page.tsx
@@ -0,0 +1,19 @@
+export default function Home() {
+  return (
+    <div className="bg-gray-500 h-screen flex items-center justify-center">
+        <h1 className="text-2xl flex items-center font-bold text-center mb-8">
+            Login
+            </h1>
+    
+        <div className="bg-white w-80 p-6 rounded-2xl shadow-lg mb-4">
+            
+        </div>
+      
+    </div>
+  );
+}
+     
+     
+    
+    
+    
