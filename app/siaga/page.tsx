2504b00baa167861134b76588f3b5b55ea5b79aa
syafikahsyafa <laylaoff0@gmail.com>
2026-06-24 20:18:31 +0800

    update login

--- app/siaga/page.tsx
+++ app/siaga/page.tsx
@@ -1,14 +1,22 @@
 export default function Home() {
   return (
-    <main className="min-h-screen flex items-center justify-center bg-gray-100">
+    <main className="min-h-screen flex items-center justify-center bg-[#ABC270]">
 
       <div className="bg-white p-8 rounded-3xl shadow-lg w-[350px]">
 
-        <h1 className="text-3xl font-bold text-center">
-          ⚡ SIAGA
-        </h1>
+        <div className="flex items-center justify-center gap-2">
+          <img
+            src="/logo_siaga_pluss.png"
+            alt="Logo"
+            className="w-8 h-8"
+          />
 
-        <p className="text-center text-gray-500 mt-2 mb-8">
+          <h1 className="text-3xl font-bold text-[#473C33]">
+            SIAGA PLUSS
+          </h1>
+        </div>
+
+        <p className="text-center text-[#473C33] mt-8 mb-8">
           Selamat Datang
         </p>
 
@@ -17,34 +25,34 @@ export default function Home() {
           <input
             type="email"
             placeholder="Email"
-            className="border rounded-xl p-3"
+            className="border border-[#FDA769] rounded-xl p-3"
           />
 
           <input
             type="password"
             placeholder="Password"
-            className="border rounded-xl p-3"
+            className="border border-[#FDA769] rounded-xl p-3"
           />
 
           <button
-            className="bg-blue-500 text-white rounded-xl p-3"
+            className="bg-[#FDA769] text-white rounded-xl p-3"
           >
             Login
           </button>
 
-          <p className="text-center text-gray-400">
+          <p className="text-center text-[#473C33]">
             atau
           </p>
 
           <button
-            className="border rounded-xl p-3"
+            className="border border-[#FDA769] rounded-xl p-3"
           >
             Masuk sebagai Tamu
           </button>
 
           <p className="text-center text-sm text-gray-500">
             Belum punya akun?
-            <span className="ml-1 text-blue-500">
+            <span className="ml-1 text-[#FDA769]">
               Daftar
             </span>
           </p>
